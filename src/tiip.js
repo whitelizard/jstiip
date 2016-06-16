f1283a89d9139fe55d7e4e102ab6d8fa66f5134e
Esbjörn Blomquist <esbjorn.blomquist@tieto.com>
2016-06-17 00:29:57 +0200

    some dependency updates and framework stuff

--- src/tiip.js
+++ src/tiip.js
@@ -2,27 +2,31 @@
 function baseMessage() {
   return {
     protocol: 'tiip.0.9',
-    timestamp: Date.now()/1000+''
+    timestamp: String(Date.now() / 1000),
   };
+}
+
+function isDef(value) {
+  return typeof value !== undefined && value !== null;
 }
 
 export function pack(
   type, target, signal, arguments_, payload,
   mid, tenant, source, sid, ok, subTarget
 ) {
-  let msg = baseMessage();
+  const msg = baseMessage();
 
-  if (typeof type !== undefined && type !== null) msg.type = type;
-  if (typeof target !== undefined && target !== null) msg.target = target;
-  if (typeof signal !== undefined && signal !== null) msg.signal = signal;
-  if (typeof arguments_ !== undefined && arguments_ !== null) msg.arguments = arguments_;
-  if (typeof payload !== undefined && payload !== null) msg.payload = payload;
-  if (typeof mid !== undefined && mid !== null) msg.mid = mid;
-  if (typeof tenant !== undefined && tenant !== null) msg.tenant = tenant;
-  if (typeof source !== undefined && source !== null) msg.source = source;
-  if (typeof sid !== undefined && sid !== null) msg.sid = sid;
-  if (typeof ok !== undefined && ok !== null) msg.ok = ok;
-  if (typeof subTarget !== undefined && subTarget !== null) msg.subTarget = subTarget;
+  if (isDef(type)) msg.type = type;
+  if (isDef(target)) msg.target = target;
+  if (isDef(signal)) msg.signal = signal;
+  if (isDef(arguments_)) msg.arguments = arguments_;
+  if (isDef(payload)) msg.payload = payload;
+  if (isDef(mid)) msg.mid = mid;
+  if (isDef(tenant)) msg.tenant = tenant;
+  if (isDef(source)) msg.source = source;
+  if (isDef(sid)) msg.sid = sid;
+  if (isDef(ok)) msg.ok = ok;
+  if (isDef(subTarget)) msg.subTarget = subTarget;
 
   return JSON.stringify(msg);
 }
